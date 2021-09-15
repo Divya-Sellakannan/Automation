c44f9a4d20f86f0dfa8044e5a93bc539ff4d8244
Divya-Sellakannan <divyasellakannan@gmail.com>
2021-09-16 03:34:40 +0530

    add script file to push and import automation for homepagecomponent metadata and import data file

---
+++ script.bat
@@ -0,0 +1,7 @@
+@echo off
+echo %1
+call sfdx force:source:push -f -u %1
+for /f %%a in (import-data/order_of_execution.txt) do (
+    echo %%a 
+    call sfdx force:data:tree:import -u %1 -p import-data/%%a
+)
