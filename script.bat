985e925368076b09b7d89a549f57bde1c5d02fe2
Divya-Sellakannan <divyasellakannan@gmail.com>
2021-09-17 17:08:31 +0530

    Revert "add script batch file and order of execution file for windows"
    This reverts commit 3f95d14af17c0d9803ef7985c48d882223bc7970.

--- script.bat
+++ script.bat
@@ -2,6 +2,6 @@
 echo %1
 call sfdx force:source:push -f -u %1
 for /f %%a in (import-data/order_of_execution.txt) do (
-    echo %%a
+    echo %%a 
     call sfdx force:data:tree:import -u %1 -p import-data/%%a
 )
